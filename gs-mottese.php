590876de2143c4a44c950490a7126b10fc008f19
Alexandra Haret <alexandra.haret@gmail.com>
2017-02-07 11:46:15 +0000

    Minor changes of content on pages gs-mottese and verduci

--- gs-mottese.php
+++ gs-mottese.php
@@ -21,7 +21,13 @@
 
               <h1>Gruppo Sportivo Mottese</h1>
 
-              <img src="images/gs-mottese.jpg" class="img-right" title="Gruppo Sportivo Mottese" /><p>Nel 1974 Francesco Verduci, in collaborazione con il prof. Giovanni Verduci, fond&ograve il Gruppo Sportivo Mottese, avviando alla pratica dell'atletica leggera tantissimi giovani del paese, con i quali partecip&ograve a numerose gare provinciali, regionali e nazionali ottenendo ovunque ottimi risultati nelle varie discipline.</p>
+              <img src="images/gs-mottese.jpg" class="img-right" title="Gruppo Sportivo Mottese" />
+
+              <p>Nel 1974 Francesco Verduci, in collaborazione con il prof. Giovanni Verduci, fond&ograve il Gruppo Sportivo Mottese, avviando alla pratica dell'atletica leggera tantissimi giovani del paese, con i quali partecip&ograve a numerose gare provinciali, regionali e nazionali ottenendo ovunque ottimi risultati nelle varie discipline.</p>
+
+              <p>Organizz&ograve diverse competizioni a Motta, dando lustro al paese e agli stessi ragazzi. Varie furono le edizioni del concorso Esercito Scuola, i campionati regionali giovanili di corsa campestre sugli spettacolari percorsi sterrati di Motta e numerose sono state le sﬁ de di marcia avvenute sul lungomare di Lazzaro.</p>
+
+              <p>Da ogni manifestazione traeva lo spunto per organizzare una gara di atletica legandola alla circostanza, come ad esempio nell'agosto 1979 quando diede vita alla "Coppa dell'Assunta" che si ripeter&agrave per ben quattordici edizioni ed in seguito inserita quale prova per i Campionati Regionali di Corsa in Montagna.</p>
 
             </div>
           </div>
